229a56aaa2d8a40e47baec7f8910cfd632026ee5
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-06 20:50:11 +0100

    Add Wallet Connection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 export default function App() {
-  const checkIfWalletIsConnected = () => {
-    const {ethereum} = window;
+  const [currentAccount, setCurrentAccount] = useState("");
 
-    if(ethereum){
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const {ethereum} = window;
+
+      if(ethereum){
+        console.log("Make sure you have metamask!");
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      
+      const accounts = await ethereum.request({method: "eth_accounts"});
+
+      if(accounts.length === 0){
+        console.log("No authorized account found");
+      } else {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      }
+    } catch(error){
+      console.log(error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
     }
   }
   
@@ -33,6 +67,13 @@ export default function App() {
           <button className="waveButton" onClick={null}>
             Wave
           </button>
+
+          {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+        
         </div>
       </div>      
     </div>
